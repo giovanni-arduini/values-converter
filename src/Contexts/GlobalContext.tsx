f9548d21b6a1b6ebfd95ffca659b0efba78fc376
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-22 11:46:49 +0200

    ADD fetch for getting currencies datas

--- src/Contexts/GlobalContext.tsx
+++ src/Contexts/GlobalContext.tsx
@@ -1,13 +1,24 @@
-import React, { createContext, useContext } from "react";
-import type { Currency } from "../types";
+import React, { createContext, useContext, useEffect } from "react";
+import type { CurrencyInput } from "../types";
+
+export interface CurrenciesResponse {
+  [currencyCode: string]: string;
+}
+
+interface Currency {
+  code: string;
+  name: string;
+}
 
 type GlobalContextType = {
   currencies: Currency[] | null;
-  startCurrency: Currency | null;
-  endCurrency: Currency | null;
+  startCurrency: CurrencyInput | null;
+  endCurrency: CurrencyInput | null;
+
   setCurrencies: React.Dispatch<React.SetStateAction<Currency[] | null>>;
-  setStartCurrency: React.Dispatch<React.SetStateAction<Currency | null>>;
-  setEndCurrency: React.Dispatch<React.SetStateAction<Currency | null>>;
+  setStartCurrency: React.Dispatch<React.SetStateAction<CurrencyInput | null>>;
+  setEndCurrency: React.Dispatch<React.SetStateAction<CurrencyInput | null>>;
+
   convert: (from: string, to: string, amount: number) => Promise<string>;
 };
 
@@ -27,10 +38,11 @@ export function useGlobalContext() {
 
 export function GlobalProvider({ children }: GlobalContextProviderProps) {
   const [currencies, setCurrencies] = React.useState<Currency[] | null>(null);
-  const [startCurrency, setStartCurrency] = React.useState<Currency | null>(
+  const [startCurrency, setStartCurrency] =
+    React.useState<CurrencyInput | null>(null);
+  const [endCurrency, setEndCurrency] = React.useState<CurrencyInput | null>(
     null
   );
-  const [endCurrency, setEndCurrency] = React.useState<Currency | null>(null);
 
   const convert = async (
     from: string,
@@ -51,6 +63,47 @@ export function GlobalProvider({ children }: GlobalContextProviderProps) {
 
     return formattedData;
   };
+
+  //   const defaultRate = async () => {
+  //     const response = await fetch("https://api.frankfurter.dev/v1/currencies");
+  //     if (!response.ok) {
+  //       throw new Error("Failed to fetch the currencies list");
+  //     }
+  //     const data: CurrenciesResponse = await response.json();
+  //     console.log(data);
+  //     setCurrencies(data);
+  //     console.log(currencies);
+  //   };
+
+  //   useEffect(() => {
+  //     defaultRate();
+  //   }, []);
+
+  const fetchCurrencies = async () => {
+    try {
+      const response = await fetch("https://api.frankfurter.dev/v1/currencies");
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch currencies. Status: ${response.status}`
+        );
+      }
+      const data: CurrenciesResponse = await response.json();
+      // Converto in array { code, name }
+      const mapped: Currency[] = Object.entries(data).map(([code, name]) => ({
+        code,
+        name,
+      }));
+      setCurrencies(mapped);
+    } catch (err) {
+      console.error("Error fetching currencies:", err);
+    }
+  };
+  console.log(currencies);
+
+  // Carico le valute al primo mount
+  useEffect(() => {
+    fetchCurrencies();
+  }, []);
 
   return (
     <GlobalContext.Provider
