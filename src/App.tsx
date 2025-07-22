63ec75bd1ff03b2125c84365ee2dbdef81dffc98
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-22 11:11:07 +0200

    ADD routes and provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { GlobalProvider } from "./Contexts/GlobalContext";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
+    </>
+  );
 }
 
 export default App;
