ccf3ccf136026bce728981ed9f66531698bf07a0
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-23 15:50:55 +0200

    ADD currencies as props to CurrencySelector

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,15 @@
 import CurrencySelector from "../Components/CurrencySelector";
+import { useGlobalContext } from "../Contexts/GlobalContext";
 
 export default function Home() {
+  const { currencies } = useGlobalContext();
+
   return (
     <>
       <section id="currenciesSelect">
         <form action="">
-          <CurrencySelector item={{ defaultCode: "Euro" }} />
-          <CurrencySelector item={{ defaultCode: "United States Dollar" }} />
+          <CurrencySelector item={{ defaultCode: "EUR", currencies }} />
+          <CurrencySelector item={{ defaultCode: "USD", currencies }} />
         </form>
       </section>
       <section id="resultsDisplay"></section>
