dbe9b99cf5a0988ca6e604cbbf6f198de3ee3aa3
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-24 10:29:20 +0200

    REFACTOR CurrencySelector components and functions props

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,17 +1,70 @@
+import { useState, useEffect } from "react";
 import CurrencySelector from "../Components/CurrencySelector";
 import { useGlobalContext } from "../Contexts/GlobalContext";
+import type { CurrencyInput } from "../types";
 
 export default function Home() {
-  const { currencies } = useGlobalContext();
+  const { currencies, convert } = useGlobalContext();
+
+  // stato dei due componenti CurrencySelector
+  const [left, setLeft] = useState<CurrencyInput>({
+    code: "EUR",
+    value: 1,
+  });
+  const [right, setRight] = useState<CurrencyInput>({
+    code: "USD",
+    value: 0,
+  });
+  // chi dei due sta chiamando la funzione di conversione
+  const [activeSide, setActiveSide] = useState<"left" | "right">("left");
+
+  useEffect(() => {
+    const doConversion = async () => {
+      try {
+        if (activeSide === "left") {
+          const result = await convert(left.code, right.code, left.value);
+          setRight((prev) => ({ ...prev, value: Number(result) }));
+        } else {
+          const result = await convert(right.code, left.code, right.value);
+          setLeft((prev) => ({ ...prev, value: Number(result) }));
+        }
+      } catch (err) {
+        console.error("Conversion error:", err);
+      }
+    };
+
+    // Evita di fare la conversione se manca qualcosa
+    if (left.code && right.code && (left.value >= 0 || right.value >= 0)) {
+      doConversion();
+    }
+  }, [left, right, activeSide, convert]);
 
   return (
     <>
       <section id="currenciesSelect">
-        <form action="">
-          <CurrencySelector item={{ defaultCode: "EUR", currencies }} />
-          <CurrencySelector item={{ defaultCode: "USD", currencies }} />
-        </form>
+        <CurrencySelector
+          code={left.code}
+          value={left.value}
+          currencies={currencies}
+          onChangeCode={(code) => setLeft((prev) => ({ ...prev, code }))}
+          onChangeValue={(value) => {
+            setLeft((prev) => ({ ...prev, value: Number(value) }));
+            setActiveSide("left");
+          }}
+        />
+
+        <CurrencySelector
+          code={right.code}
+          value={right.value}
+          currencies={currencies}
+          onChangeCode={(code) => setRight((prev) => ({ ...prev, code }))}
+          onChangeValue={(value) => {
+            setRight((prev) => ({ ...prev, value: Number(value) }));
+            setActiveSide("right");
+          }}
+        />
       </section>
+
       <section id="resultsDisplay"></section>
     </>
   );
