214c155a763dff28b7964b22a00d56987ddd510a
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-24 10:28:26 +0200

    FIX props to CurrencySelector

--- src/Components/CurrencySelector.tsx
+++ src/Components/CurrencySelector.tsx
@@ -1,43 +1,41 @@
-import { useState, useEffect } from "react";
 import type { Currency } from "../types";
 
+// Passo come props all'input il valore, il codice valuta e la funzione onChangeValue
+// e alla select la lista di valute e la funzone onChangeCode
 type CurrencySelectorProps = {
-  item: { defaultCode: string; currencies: Currency[] | null };
+  code: string;
+  value: number;
+  currencies: Currency[] | null;
+  onChangeCode: (code: string) => void;
+  onChangeValue: (value: string) => void;
 };
 
-export default function CurrencySelector({ item }: CurrencySelectorProps) {
-  const { defaultCode, currencies } = item;
-  const [value, setValue] = useState(defaultCode);
-
-  useEffect(() => {
-    if (currencies && currencies.some((curr) => curr.code === defaultCode)) {
-      setValue(defaultCode);
-    }
-  }, [currencies, defaultCode]);
-
-  const handleSelectChange = (
-    e: React.ChangeEvent<HTMLSelectElement>
-  ): void => {
-    setValue(e.target.value);
-  };
+export default function CurrencySelector({
+  code,
+  value,
+  currencies,
+  onChangeCode,
+  onChangeValue,
+}: CurrencySelectorProps) {
+  const isValidDecimal = /^[0-9]\d+(\.\d+)?$/.test(value.toString());
 
   return (
-    <>
-      <div className="">
-        <select
-          name="currencySelector"
-          value={value}
-          onChange={handleSelectChange}
-        >
-          {currencies?.map((curr) => {
-            return (
-              <option key={curr.code} value={curr.code}>
-                {curr.name}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-    </>
+    <div>
+      <input
+        type="text"
+        pattern="^[0-9]\d+(\.\d+)?$"
+        inputMode="decimal"
+        autoComplete="off"
+        value={value}
+        onChange={(e) => onChangeValue(e.target.value)}
+      />
+      <select value={code} onChange={(e) => onChangeCode(e.target.value)}>
+        {currencies?.map((curr) => (
+          <option key={curr.code} value={curr.code}>
+            {curr.name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
