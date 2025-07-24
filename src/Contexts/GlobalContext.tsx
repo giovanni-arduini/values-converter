f4729350089314f5096f9aed985a2d99fee42452
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-24 12:07:16 +0200

    ADD fetch function for conversionHistory

--- src/Contexts/GlobalContext.tsx
+++ src/Contexts/GlobalContext.tsx
@@ -5,12 +5,18 @@ export interface CurrenciesResponse {
   [currencyCode: string]: string;
 }
 
+export interface CurrencyHistoryResponse {
+  values: {};
+}
+
 type GlobalContextType = {
   currencies: Currency[] | null;
+  currencyHistory: {};
   startCurrency: CurrencyInput | null;
   endCurrency: CurrencyInput | null;
 
   setCurrencies: React.Dispatch<React.SetStateAction<Currency[] | null>>;
+  setCurrencyHistory: React.Dispatch<React.SetStateAction<{}>>;
   setStartCurrency: React.Dispatch<React.SetStateAction<CurrencyInput | null>>;
   setEndCurrency: React.Dispatch<React.SetStateAction<CurrencyInput | null>>;
 
@@ -33,6 +39,7 @@ export function useGlobalContext() {
 
 export function GlobalProvider({ children }: GlobalContextProviderProps) {
   const [currencies, setCurrencies] = React.useState<Currency[] | null>(null);
+  const [currencyHistory, setCurrencyHistory] = React.useState<{}>({});
   const [startCurrency, setStartCurrency] =
     React.useState<CurrencyInput | null>(null);
   const [endCurrency, setEndCurrency] = React.useState<CurrencyInput | null>(
@@ -58,6 +65,25 @@ export function GlobalProvider({ children }: GlobalContextProviderProps) {
     const formattedData = (amount * data.rates[to]).toFixed(2).toString();
 
     return formattedData;
+  };
+
+  // Funzione per ottenere storico tra due valute
+
+  const fetchConversionHistory = async (code: string) => {
+    try {
+      const response = await fetch(
+        " https://api.frankfurter.dev/v1/2024-01-01..?symbols=USD"
+      );
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch conversion history for ${code}. Status:${response.status}`
+        );
+      }
+      const data: CurrencyHistoryResponse = await response.json();
+      setCurrencyHistory(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // Funzione per ottenere la lista delle valute -
@@ -91,6 +117,8 @@ export function GlobalProvider({ children }: GlobalContextProviderProps) {
       value={{
         currencies,
         setCurrencies,
+        currencyHistory,
+        setCurrencyHistory,
         startCurrency,
         setStartCurrency,
         endCurrency,
