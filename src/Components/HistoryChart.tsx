376e0f66675f9d99a35faedba91d12c845f9888c
giovanni-arduini <gionnibigudd@gmail.com>
2025-07-24 17:35:05 +0200

    FIX xasis tickAmount for better readability

--- src/Components/HistoryChart.tsx
+++ src/Components/HistoryChart.tsx
@@ -18,28 +18,37 @@ const HistoryChart: React.FC = () => {
     currencyHistory,
   } = useGlobalContext();
 
-  let data: number[] = [];
   let categories: string[] = [];
+  let data: number[] = [];
 
   if (currencyHistory) {
-    categories = Object.keys(currencyHistory.rates); // ["2023-12-29", "2024-01-02", "2024-01-03"]
+    categories = Object.keys(currencyHistory.rates).map((dateStr) => {
+      const date = new Date(dateStr);
+      return new Intl.DateTimeFormat("it-IT").format(date); // array di stringhe con formato di data italiana
+    });
+
     data = Object.values(currencyHistory.rates).map(
       (rateObj) => Object.values(rateObj)[0]
-    ); // [1.1, 1.12, 1.15]
+    ); // Array di numeri decimali
   }
 
+  //
   const series: SeriesType = [
     {
-      name: "series-1",
+      name: "conversion-rate",
       data: data,
     },
   ];
 
   const options: ApexOptions = {
     chart: {
-      id: "basic-bar",
+      id: "currency-conversion",
     },
+    // dataLabels: { enabled: true },
     xaxis: {
+      tickAmount: 20,
+      // range: 390,
+      // stepSize: 10,
       categories: categories,
     },
   };
